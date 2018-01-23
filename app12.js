c1ef0298d523c06abc764f7e69053e3d257d9339
Young Min Song <ysong88@myseneca.ca>
2018-01-23 17:35:06 -0500

    Change Files

--- app12.js
+++ app12.js
@@ -1,137 +1,127 @@
-/**
- * session 사용하기
- * 
- * 웹브라우저에서 아래 주소의 페이지를 열고 웹페이지에서 요청
- *    http://localhost:3000/public/login2.html
- *
- * @date 2016-10-25
- * @author Mike
- */
-
-// Express 기본 모듈 불러오기
+// Loading basic Express module
 var express = require('express')
-  , http = require('http')
-  , path = require('path');
+    , http = require('http')
+    , path = require('path');
 
-// Express의 미들웨어 불러오기
+// Loading Express middleware
 var bodyParser = require('body-parser')
-  , cookieParser = require('cookie-parser')
-  , static = require('serve-static')
-  , errorHandler = require('errorhandler');
-
-// 에러 핸들러 모듈 사용
+    , static = require('serve-static')
+    // Loading cookie-parser middleware
+    , cookieParser = require('cookie-parser');
+    // , errorHandler = require('errorhandler');
+    
+// Using error-handler module
 var expressErrorHandler = require('express-error-handler');
 
-// Session 미들웨어 불러오기
-var expressSession = require('express-session');
+// Loading Session middleware
+var expressSession = require("express-session");
 
-
-// 익스프레스 객체 생성
+// Create Express object
 var app = express();
 
-// 기본 속성 설정
+// Configure the basic port
 app.set('port', process.env.PORT || 3000);
 
-// body-parser를 이용해 application/x-www-form-urlencoded 파싱
+// Using body-parser to parsing "application/x-www-form-urlencoded"
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// body-parser를 이용해 application/json 파싱
+// Using body-parser parsing application/json
 app.use(bodyParser.json())
-
 app.use('/public', static(path.join(__dirname, 'public')));
 
-// cookie-parser 설정
+// cookie-parser setting
 app.use(cookieParser());
 
-// 세션 설정
+// Session setting
 app.use(expressSession({
-	secret:'my key',
-	resave:true,
-	saveUninitialized:true
+    secret:'my key',
+    resave: true,
+    saveUninitialized: true
 }));
 
-
-// 라우터 사용하여 라우팅 함수 등록
+// Router Object
 var router = express.Router();
 
-// 로그인 라우팅 함수 - 로그인 후 세션 저장함
-router.route('/process/login').post(function(req, res) {
-	console.log('/process/login 호출됨.');
-
-	var paramId = req.body.id || req.query.id;
-	var paramPassword = req.body.password || req.query.password;
-	
-	if (req.session.user) {
-		// 이미 로그인된 상태
-		console.log('이미 로그인되어 상품 페이지로 이동합니다.');
-		
-		res.redirect('/public/product.html');
-	} else {
-		// 세션 저장
-		req.session.user = {
-			id: paramId,
-			name: '소녀시대',
-			authorized: true
-		};
-		
-		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-		res.write('<h1>로그인 성공</h1>');
-		res.write('<div><p>Param id : ' + paramId + '</p></div>');
-		res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
-		res.write("<br><br><a href='/process/product'>상품 페이지로 이동하기</a>");
-		res.end();
-	}
-});
-
-// 로그아웃 라우팅 함수 - 로그아웃 후 세션 삭제함
-router.route('/process/logout').get(function(req, res) {
-	console.log('/process/logout 호출됨.');
-	
-	if (req.session.user) {
-		// 로그인된 상태
-		console.log('로그아웃합니다.');
-		
-		req.session.destroy(function(err) {
-			if (err) {throw err;}
-			
-			console.log('세션을 삭제하고 로그아웃되었습니다.');
-			res.redirect('/public/login2.html');
-		});
-	} else {
-		// 로그인 안된 상태
-		console.log('아직 로그인되어있지 않습니다.');
-		
-		res.redirect('/public/login2.html');
-	}
-});
-
-// 상품정보 라우팅 함수
+// Routing function for product information
 router.route('/process/product').get(function(req, res) {
-	console.log('/process/product 호출됨.');
-	
-	if (req.session.user) {
-		res.redirect('/public/product.html');
-	} else {
-		res.redirect('/public/login2.html');
-	}
+    console.log('/process/product called.');
+
+    if(req.session.user) {
+        res.redirect('/public/product.html');
+    } else {
+        res.redirect('/public/login2.html');
+    }
 });
 
+// Login Routing function - After login, session will save
+router.route('/process/login').post(function(req, res) {
+    console.log('/process/login called.');
+
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+
+    if (req.session.user) {
+        // If already logged in,
+        console.log('Already logged in. Move to product information page.');
+
+        res.redirect('/public/product.html');
+    } else {
+        // Save session
+        req.session.user = {
+            id: paramId,
+            name: 'Song',
+            authorized: true
+        };
+
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+        res.write('<h1>Log in Success!</h1>');
+        res.write('<div><p>Param id : ' + paramId + '</p></div>');
+        res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
+        res.write("<br><br><a href='/process/product'>Move to the product information page.</a>");
+        res.end();
+    }
+});
+
+// After log out, session will be deleted.
+router.route('/process/logout').get(function(req, res) {
+    console.log('/process/logout 호출됨.');
+
+    if (req.session.user) {
+        // If already logged in,
+        console.log('Logging out.');
+
+        req.session.destroy(function(err) {
+            if(err) {throw err;}
+
+            console.log('Log out successed. Session will be delete.');
+            res.redirect('/public/login2.html');
+        });
+    } else {
+        // If not logged in,
+        console.log('You are not log in yet.');
+
+        res.redirect('/public/login2.html');
+    }
+});
+
+// Adding Router object to app object
 app.use('/', router);
 
+// Error page for unregistered path
+app.all('*', function(req, res) {
+    res.status(404).send('<h1>ERROR - Could not find the page.</h1>');
+});
 
-// 404 에러 페이지 처리
+// After routed, 404 error page handled.
 var errorHandler = expressErrorHandler({
     static: {
-      '404': './public/404.html'
+        '404': './public/404.html'
     }
 });
 
 app.use( expressErrorHandler.httpError(404) );
 app.use( errorHandler );
 
-
-// Express 서버 시작
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
-
